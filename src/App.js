bf1c15e072bbb1ede2a5a70b90f2572835fc2b9f
unknown <muddasir@tridz.com>
2020-09-02 22:12:32 +0530

    2.0

--- src/App.js
+++ src/App.js
@@ -7,8 +7,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          SOLARHOMES
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Editwwwwwwwww <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
