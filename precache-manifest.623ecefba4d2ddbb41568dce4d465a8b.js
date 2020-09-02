307b0d06932fd26daf0fbb6e3339d87ff4e66086
unknown <muddasir@tridz.com>
2020-09-02 21:31:40 +0530

    Updates

---
+++ precache-manifest.623ecefba4d2ddbb41568dce4d465a8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "377f1966218ff8b7579db48b526f2509",
+    "url": "/kaimudu.github.io/index.html"
+  },
+  {
+    "revision": "85b62d5656f5851f2485",
+    "url": "/kaimudu.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "901583ee10f65197a134",
+    "url": "/kaimudu.github.io/static/js/2.c24a221a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kaimudu.github.io/static/js/2.c24a221a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85b62d5656f5851f2485",
+    "url": "/kaimudu.github.io/static/js/main.498bb22f.chunk.js"
+  },
+  {
+    "revision": "bdb3ae8e0104010e1e69",
+    "url": "/kaimudu.github.io/static/js/runtime-main.1e9c745a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kaimudu.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
